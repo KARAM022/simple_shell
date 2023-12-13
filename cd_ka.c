26b32754be83dac20ebc07fa521a60ca67e546a5
KARAM022 <my0fav0library@gmail.com>
2023-12-13 02:55:11 +0100

    test

--- cd_ka.c
+++ cd_ka.c
@@ -3,44 +3,45 @@
 int cd_f(char **command)
 {
     char cwd[1024];
+    
     if (command[1] == NULL || _strcmp(command[1], "~") == 0)
+    {
+        if (chdir(getenv("HOME")) != 0)
         {
-            if (chdir(getenv("HOME")) != 0)
-            {
-                perror("chdir");
-            }
+            perror("chdir");
         }
-        else if (_strcmp(command[1], "-") == 0)
+    }
+    else if (_strcmp(command[1], "-") == 0)
+    {
+        char *p_dir = getenv("OLDPWD");
+        if (p_dir == NULL)
         {
-            char *p_dir = getenv("OLDPWD");
-            if (p_dir == NULL)
-            {
-                fprintf(stderr, "OLDPWD not set\n");
-            }
-            else
-            {
-                if (chdir(p_dir) != 0)
-                {
-                    perror("chdir");
-                }
-            }
+            fprintf(stderr, "OLDPWD not set\n");
         }
         else
         {
-            if (chdir(command[1]) != 0)
+            if (chdir(p_dir) != 0)
             {
                 perror("chdir");
             }
         }
+    }
+    else
+    {
+        if (chdir(command[1]) != 0)
+        {
+            perror("chdir");
+        }
+    }
 
-        if (getcwd(cwd, sizeof(cwd)) != NULL)
-        {
-            setenv("PWD", cwd, 1);
-        }
-        else
-        {
-            perror("getcwd");
-        }
-        freearr(command);
-        return (0);
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        setenv("PWD", cwd, 1);
+    }
+    else
+    {
+        perror("getcwd");
+    }
+    freearr(command);
+    return (0);
 }
