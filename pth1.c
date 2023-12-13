2452897169eb4c793b70c02f27c14d9e9f012771
KARAM022 <my0fav0library@gmail.com>
2023-12-13 20:00:59 +0100

    betty

--- pth1.c
+++ pth1.c
@@ -3,10 +3,10 @@
 char pth(const char *inputcommand)
 {
     if (inputcommand[i] == '/')
-		{
-			if (stat(inputcommand, &st) == 0)
-				return (s_dup(inputcommand));
-			return (NULL);
-		}
-		i++;
+    {
+        if (stat(inputcommand, &st) == 0)
+            return (s_dup(inputcommand));
+        return (NULL);
+    }
+    i++;
 }
