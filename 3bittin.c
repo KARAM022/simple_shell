075bc4071e57b017ccacdded93516ce59d2137db
ashrafbel <ashrafmus2@gmail.com>
2023-12-12 16:28:43 -0800

    s

---
+++ 3bittin.c
@@ -0,0 +1,79 @@
+#include "shell.h"
+
+int handle_builtin_commands(char **command, char **av, int idx,int status)
+{
+        char cwd[1024];
+
+    if (_strcmp(command[0], "cd") == 0)
+    {
+        if (command[1] == NULL || _strcmp(command[1], "~") == 0)
+        {
+            if (chdir(getenv("HOME")) != 0)
+            {
+                perror("chdir");
+            }
+        }
+        else if (_strcmp(command[1], "-") == 0)
+        {
+            char *p_dir = getenv("OLDPWD");
+            if (p_dir == NULL)
+            {
+                fprintf(stderr, "OLDPWD not set\n");
+            }
+            else
+            {
+                if (chdir(p_dir) != 0)
+                {
+                    perror("chdir");
+                }
+            }
+        }
+        else
+        {
+            if (chdir(command[1]) != 0)
+            {
+                perror("chdir");
+            }
+        }
+
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+            setenv("PWD", cwd, 1);
+        }
+        else
+        {
+            perror("getcwd");
+        }
+        freearr(command);
+        return(0);
+    }
+    else if (_strcmp(command[0], "exit") == 0)
+    {
+        int exit_status = 0;
+
+        if (command[1] != NULL)
+        {
+            exit_status = atoi(command[1]);
+        }
+        else
+            exit_status =status;
+        
+        freearr(command);
+        exit(exit_status);
+    }
+    else if (_strcmp(command[0], "env") == 0)
+    {
+        char **env = environ;
+
+        while (*env != NULL)
+        {
+            write(STDOUT_FILENO, *env, strlen(*env));
+            write(STDOUT_FILENO, "\n", 1);
+            env++;
+        }
+        freearr(command);
+        return 0;
+    }
+        return execute(command, av, idx);
+
+}
