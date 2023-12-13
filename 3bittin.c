f909d7138b5c36fb96b5722d1260b7c555bdc38a
KARAM022 <my0fav0library@gmail.com>
2023-12-13 02:30:47 +0100

    test

--- 3bittin.c
+++ 3bittin.c
@@ -6,7 +6,7 @@ int h_built_in(char **command, char **av, int idx, int status)
 
     if (_strcmp(command[0], "cd") == 0)
     {
-        int cd_f(char **command);
+        return int cd_f(char **command);
     }
     else if (_strcmp(command[0], "exit") == 0)
     {
