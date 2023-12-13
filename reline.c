460e1a9f82a7b82eff72c1b67737d941df84de31
ashrafbel <ashrafmus2@gmail.com>
2023-12-13 12:12:42 -0800

    reline

--- reline.c
+++ reline.c
@@ -3,21 +3,23 @@
  * readline - function readline
  * Return: char or null if an error or if eof is reched
  */
-char *readline(void)
-{
-char *line = NULL;
-size_t len = 0;
-ssize_t rb;
+#include "shell.h"
 
-if (isatty(STDIN_FILENO))
-{
-write(STDOUT_FILENO, "$ ", 2);
-}
-rb = getline(&line, &len, stdin);
-if (rb == EOF)
-{
-free(line);
-return (NULL);
-}
-return (line);
+char *readline(void) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t n;
+
+    if (isatty(STDIN_FILENO)) {
+        write(STDOUT_FILENO, "$ ", 2);
+    }
+
+    n = getline(&line, &len, stdin);
+
+    if (n == EOF) {
+        free(line);
+        return NULL;
+    }
+
+    return line;
 }
