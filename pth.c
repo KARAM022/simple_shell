bb1b4b77520aaa5690b282311dfb5127a4b07d2f
ashrafbel <ashrafmus2@gmail.com>
2023-12-13 13:28:14 -0800

    path

--- pth.c
+++ pth.c
@@ -1,56 +1,45 @@
 #include "shell.h"
-#include <sys/stat.h>
-
 /**
- * _getApTH - function
- * @inputcommand:variable
- * Return: char
+ * _getApTH - Searches for the full path of a command
+ * @inputcommand:command to search for
+ * Return: pointer to the full path if found otherwise NULL
  */
+char *_getApTH(const char *inputcommand)
+{
+char *PENVTH, *FCDM, *D;
+struct stat st;
+int i = 0;
 
-char *_getApTH(const char *inputcommand) {
-    char *PENVTH, *FCDM, *D;
-    struct stat st;
-
-   int i = 0;
-while (inputcommand && inputcommand[i]) {
-    if (inputcommand[i] == '/') {
-        if (stat(inputcommand, &st) == 0) {
-            return s_dup(inputcommand);
-        }
-        return NULL;
-    }
-    i++;
+while (inputcommand && inputcommand[i])
+{
+if (inputcommand[i] == '/')
+{
+if (stat(inputcommand, &st) == 0)
+{
+return (s_dup(inputcommand));
 }
-
-
-    PENVTH = my_getenv("PATH");
-
-    if (!PENVTH)
-        return NULL;
-
-    D = strtok(PENVTH, ":");
-
-    while (D) {
-        FCDM = malloc(_strlen(D) + _strlen(inputcommand) + 2);
-
-        if (FCDM) {
-            s_cpy(FCDM, D);
-            s_cat(FCDM, "/");
-
-            s_cat(FCDM, inputcommand);
-
-            if (stat(FCDM, &st) == 0) {
-                free(PENVTH);
-                return FCDM;
-            }
-
-            free(FCDM);
-            FCDM = NULL;
-            D = strtok(NULL, ":");
-        }
-    }
-
-    free(PENVTH);
-    return NULL;
+return (NULL);
 }
-
+i++;
+}
+PENVTH = my_getenv("PATH");
+if (!PENVTH)
+return (NULL);
+D = strtok(PENVTH, ":");
+while (D)
+{
+FCDM = malloc(_strlen(D) + _strlen(inputcommand) + 2);
+if (FCDM)
+{
+s_cpy(FCDM, D), s_cat(FCDM, "/"), s_cat(FCDM, inputcommand);
+if (stat(FCDM, &st) == 0)
+{
+free(PENVTH);
+return (FCDM);
+}
+free(FCDM), FCDM = NULL, D = strtok(NULL, ":");
+}
+}
+free(PENVTH);
+return (NULL);
+}
