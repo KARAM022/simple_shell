a1d99a281aea3f14657ade84f3ef63de61f592d5
ashrafbel <ashrafmus2@gmail.com>
2023-12-12 16:33:22 -0800

    s

---
+++ reline.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+char *readline(void) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t n;
+
+    if (isatty(STDIN_FILENO)) {
+        write(STDOUT_FILENO, "$ ", 2);
+    }
+
+    n = getline(&line, &len, stdin);
+
+    if (n == EOF) {
+        free(line);
+        return NULL;
+    }
+
+    return line;
+}
